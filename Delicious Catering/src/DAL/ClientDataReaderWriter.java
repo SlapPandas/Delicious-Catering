77576e6ece18b83ab091c8262882543e6d0f9d07
Matin De Wet <matindewet116@gmail.com>
2021-02-26 08:27:03 +0200

    Updated Client File ReaderWriter

--- Delicious Catering/src/DAL/ClientDataReaderWriter.java
+++ Delicious Catering/src/DAL/ClientDataReaderWriter.java
@@ -3,6 +3,12 @@ package DAL;
 public class ClientDataReaderWriter implements ReadWriteInterface {
     /*
         Text File information formatting
-        UserName^Password^Name^Surname^Balance^Address^IsAdmin
+        UserName^Password^Name^Surname^PhoneNumber^Email^Address^Balance^IsAdmin
     */
+    public String FileReader() {
+        return "";
+    }
+    public boolean FileWriter(String input) {
+        return true;
+    }
 }
