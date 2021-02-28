8492b273716d3315e0e012dc6c42be2662463e56
Matin De Wet <matindewet116@gmail.com>
2021-02-28 11:22:29 +0200

    Implementer writing to files
    implementation for reading and writing to all files are complete, but orders do not work

--- Delicious Catering/src/DAL/BeverageData.java
+++ Delicious Catering/src/DAL/BeverageData.java
@@ -1,11 +1,12 @@
 package DAL;
 import java.util.*;
+import javax.swing.JOptionPane;
 import BLL.*;
 
 public class BeverageData {
-    public List<Beverage> BeveragesList()
+    public List<Beverage> ReadBeveragesList()
     {
-        BeverageDataReaderWriter myReader= new BeverageDataReaderWriter();
+        DataReaderWriter myReader= new DataReaderWriter("Beverage");
         List<Beverage> myBeverageList = new ArrayList<>();
         List<String> myBeverageStringList = myReader.FileReader();
 
@@ -14,5 +15,12 @@ public class BeverageData {
             myBeverageList.add(new Beverage(myHoldingString[0], Double.parseDouble(myHoldingString[1])));
         }
         return myBeverageList;
+    }
+    public void WriteNewBeverage(String name,Double price)
+    {
+        DataReaderWriter myWriter= new DataReaderWriter("Beverage");
+        if(myWriter.FileWriter(name+"#"+price)!=true){
+            JOptionPane.showMessageDialog(null, "Could not write beverage To file");
+        }
     }
 }
