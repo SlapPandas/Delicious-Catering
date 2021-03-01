726ffb3bee6f14e5e23ff119657733279c396f9e
Matin De Wet <matindewet116@gmail.com>
2021-03-01 22:26:15 +0200

    removed all comments from app.java

--- Delicious Catering/src/App.java
+++ Delicious Catering/src/App.java
@@ -1,35 +1,5 @@
-import java.util.*;
-
-import BLL.*;
-import DAL.*;
-import PL.*;
-import java.text.*;
-
 public class App {
     public static void main(String[] args) throws Exception {
-
-        //Order myOrder = new Order();
-
-        //SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-        //        //Date myDate = formatter.parse("10-05-2021");
-        //        String myDate = "21-03-2021";
-
-        //System.out.println(myOrder.CheckDeposit(1,"Blue"));
-        //ClientCreateBooking test2 = new ClientCreateBooking();
-        //test2.createNewBooking();
-        //OrderData the = new OrderData();
-        //List<Order> myOrder = the.ReadOrderList();
-        //the.UpdateOrder(myOrder.get(0).getOrdernr(), myOrder.get(0).getClientName(), myOrder.get(0).getEventAddress(), myOrder.get(0).getEventType(), myOrder.get(0).getDecoration(), myOrder.get(0).getTheme(), myOrder.get(0).getEventDate(), myOrder.get(0).getFood(), myOrder.get(0).getBeverages(), myOrder.get(0).getSpecialFoodRequest(), myOrder.get(0).getAdultFood_TotalCost(), myOrder.get(0).getChildFood_TotalCost(), myOrder.get(0).getBevarages_TotalCost(), myOrder.get(0).getDecoration_TotalCost(), myOrder.get(0).getAddOns(), myOrder.get(0).getAddOns_TotalCost(), myOrder.get(0).getCovidEquipment(), myOrder.get(0).getCovidEquip_TotalCost(), false, myOrder.get(0).getTotalCost(), myOrder.get(0).getDepositDue(), myOrder.get(0).getDepositPaid(), myOrder.get(0).getRemainingAmount(), myOrder.get(0).getChildrenAttending(), myOrder.get(0).getAdultsAttending());
-        //Food mf = new Food();
-        //List<Food> myfood = mf.GetFood("Main");
-        //for(int i =0;i<myfood.size();i++){
-        //    System.out.println(myfood.get(i).getFoodName() +"       "+ myfood.get(i).getFoodCourseType());
-        //}
-
-        Client c = new Client();
-
-        c.RegisterClient("m1", "paswrd", "Matin", "deWet", "0123456789", "m@gmail.com", "waterkloof", false);
-
+        System.out.println("Hello, World!");
     }
 }
-
