dc95dc052980a0d8c3580635f2bda685a9926f86
Matin De Wet <matindewet116@gmail.com>
2021-03-01 09:42:51 +0200

    Correcter Food Data

--- Delicious Catering/src/DAL/FoodData.java
+++ Delicious Catering/src/DAL/FoodData.java
@@ -12,18 +12,18 @@ public class FoodData {
 
         for(int i=0;i<myFoodStringList.size();i++){
             String[] myHoldingString = myFoodStringList.get(i).split("#");
-            myFoodList.add(new Food(Course(myHoldingString[0]),myHoldingString[1], Double.parseDouble(myHoldingString[2])));
+            myFoodList.add(new Food(CourseFullName(myHoldingString[0]),myHoldingString[1], Double.parseDouble(myHoldingString[2])));
         }
         return myFoodList;
     }
     public void WriteNewFood(String course,String name,Double price)
     {
         DataReaderWriter myWriter= new DataReaderWriter("Food");
-        if(myWriter.FileWriter(course+"#"+name+"#"+price)!=true){
+        if(myWriter.FileWriter(CourseShortName(course)+"#"+name+"#"+price)!=true){
             JOptionPane.showMessageDialog(null, "Could not write beverage To file");
         }
     }
-    private String Course(String input){
+    private String CourseFullName(String input){
         String output = "";
         switch(input){
             case "S":
@@ -32,6 +32,18 @@ public class FoodData {
             output = "Main";
             case "D":
             output = "Dessert";
+        }
+        return output;
+    }
+    private String CourseShortName(String input){
+        String output = "";
+        switch(input){
+            case "Starter":
+            output = "S";
+            case "Main":
+            output = "M";
+            case "Dessert":
+            output = "D";
         }
         return output;
     }
