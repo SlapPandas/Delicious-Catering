92040344e575992aaf8a4aa440852ca5553f404f
JaydenHardman <Jayden.hardman.work@gmail.com>
2021-03-04 17:43:56 +0200

    Finished clientMenu

--- Delicious Catering/src/PL/ClientMenu.java
+++ Delicious Catering/src/PL/ClientMenu.java
@@ -1,23 +1,22 @@
 package PL;
 
+import BLL.*;
 import java.text.ParseException;
 import java.util.*;
 
-import BLL.Order;
-
-
 public class ClientMenu {
     Scanner stringInput = new Scanner(System.in);
-    Order OV = new Order();
-    public void runClientMenu(){        
-        clearScreen();
+    Order OV = new Order();  
+    ClientCreateBooking NB = new ClientCreateBooking();  
+    public void runClientMenu(String username){
+        Boolean exitInput = false;      
+        while (exitInput == false) {
+            clearScreen();
         printClientMenu();
         try {
-            clientMenuChoice(clientMenuInput());
-        } catch (Exception e) {
-
-        }
-        
+            clientMenuChoice(clientMenuInput(), username);
+        } catch (Exception e) {}
+        }                
     }
 
     private void printClientMenu(){
@@ -47,25 +46,21 @@ public class ClientMenu {
         return choice;
     }
 
-    private void clientMenuChoice(int choice) throws ParseException{
+    private void clientMenuChoice(int choice, String username) throws ParseException{
         Boolean validInput = false;
         while(validInput == false){
             switch(choice){
                 case 0:
                     System.exit(0);                                    
                     break;
-                case 1: 
-                    ClientCreateBooking NB = new ClientCreateBooking();
-                    NB.createNewBooking();
-                    
+                case 1:                     
+                    NB.createNewBooking();                    
                     break;
                 case 2:                
-                    cancelBooking();
-                    
+                    cancelBooking(username);                    
                     break;
                 case 3:
-                    payBooking();
-                    
+                    payBooking(username);                    
                     break;
                 case 4:
                     validInput = true;
@@ -78,45 +73,43 @@ public class ClientMenu {
         }    
     }
 
-    private void cancelBooking(){
-
-    }
-
-    private void payBooking(){
-        int orderID;
-        String username;
-        Boolean validInput = false;
-        String input;
-        input = stringInput.nextLine();
-        while(validInput == false){  
-            System.out.println("Please enter your username:");
-                    
-            if(input == null|| input.matches("")){
-                System.out.println("Please enter a valid input");
-                input = stringInput.nextLine();
-                input = input.trim();
+    private void cancelBooking(String username) throws ParseException{
+        System.out.println("Please enter your order number");
+        String ordernrInput;
+        Boolean validNumber = false;
+        int ordernr = 0;
+        while (validNumber == false) {
+            ordernrInput = stringInput.nextLine().trim();
+            try {
+                ordernr = Integer.parseInt(ordernrInput);
+                validNumber = true;
+            } catch (Exception e) {
+                System.out.println("Invalid input, please try again");
             }
-            else
-                validInput = true;
-        }   
-        validInput = false;
-        Boolean validID = false;
-        while (validID == false) {
-            while (validInput == false) {                
-                System.out.println("Please enter your order ID");   
-                input = stringInput.nextLine();
-            if(input == null|| input.matches("")){
-                System.out.println("Please enter a valid input");
-                input = stringInput.nextLine();
-                input = input.trim();
-            }
-            else
-                validInput = true;
-            }
-
         }
+        OV.Cancellation(ordernr, username);
     }
 
+    private void payBooking(String username){
+        int ordernr = 0;
+        Boolean validInput = false;
+        String ordernrInput;
+        
+        while (validInput == false) {            
+            System.out.println("Please enter your order number");
+            ordernrInput = stringInput.nextLine().trim();
+            try {
+                ordernr = Integer.parseInt(ordernrInput);
+            } catch (Exception e) {
+                System.out.println("Please enter a valid input");
+            }
+        }
+        try {
+            OV.CheckDeposit(ordernr, username);
+        } catch (Exception e) {
+            System.out.println("unexpected error");
+        }        
+    }
     private static void clearScreen(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
--- Delicious Catering/src/PL/LoginMenu.java
+++ Delicious Catering/src/PL/LoginMenu.java
@@ -1,8 +1,16 @@
 package PL;
 
 public class LoginMenu {
-
+    ClientMenu CM = new ClientMenu();
     public static void runLoginMenu(){
+        ClientMenu CM = new ClientMenu();
         
+        
+        // login client = true
+        String username = "Test";
+        CM.runClientMenu(username);
+
+        // login admin
+
     }
 }
