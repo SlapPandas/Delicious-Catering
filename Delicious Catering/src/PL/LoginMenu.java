c875daeb9f30b904fa8f36ecb99954c9711d0e56
JaydenHardman <Jayden.hardman.work@gmail.com>
2021-03-06 14:32:58 +0200

    client login working. need to finish admin.

--- Delicious Catering/src/PL/LoginMenu.java
+++ Delicious Catering/src/PL/LoginMenu.java
@@ -1,16 +1,48 @@
 package PL;
 
-public class LoginMenu {
-    ClientMenu CM = new ClientMenu();
-    public static void runLoginMenu(){
-        ClientMenu CM = new ClientMenu();
-        
-        
-        // login client = true
-        String username = "Test";
-        CM.runClientMenu(username);
+import BLL.*;
+import java.util.*;
 
-        // login admin
-        // view all order, clients, food, drinks, addons, add to the files(just check types)
+public class LoginMenu {
+    Scanner stringInput = new Scanner(System.in); 
+    ClientMenu CM = new ClientMenu();
+    
+
+    public void runLoginMenu(){
+        ClientMenu CM = new ClientMenu();        
+        Boolean validLogin = false, isAdmin = false;
+        String username = "",password="";
+        Client LC = new Client();
+
+        while (!validLogin) {
+            System.out.println("Please enter your username:");
+            username = checkValidStringInput(stringInput.nextLine().trim());
+            System.out.println("Please enter password:");
+            password = checkValidStringInput(stringInput.nextLine().trim());
+            validLogin = LC.SuccessfulLogin(username, password);
+            if (!validLogin) {
+                System.out.println("Please enter a valid username and password.");
+            }
+        }
+        if (isAdmin) {
+            // run admin logic
+            // view all order, clients, food, drinks, addons, add to the files(just check types)
+        } else {
+            CM.runClientMenu(username);
+        }                        
+    }
+
+    private String checkValidStringInput(String input){
+        Boolean validInput = false;
+        while(validInput == false){            
+            if(input ==null|| input.matches("")){
+                System.out.println("Please enter a valid input");
+                input = stringInput.nextLine();
+                input = input.trim();                
+            }
+            else
+                validInput = true;                
+        }
+        return input;
     }
 }
